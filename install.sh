07572f88b75ddb9ed55b4bcffd38d11f1b153103
zhaoxudong <zzxddyx@163.com>
2019-03-14 08:34:34 +0800

    新文件：   install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#########################################################################
+# File Name: install.sh
+# Author: BernieZhao
+# mail: berniezhao@ezfun.cn
+# Created Time: 2018年12月08日 星期六 09时57分34秒
+#########################################################################
+
+#C语言库函数基本的帮助文档
+sudo apt-get install manpages
+sudo apt-get install manpages-de
+sudo apt-get install manpages-de-dev
+sudo apt-get install manpages-dev
+
+#POSIX函数帮助文档
+sudo apt-get install manpages-posix
+sudo apt-get install manpages-posix-dev
+
+#内核函数
+sudo apt-get install linux-doc
+sudo apt-get install libcorelinux-doc
+
+#C++
+sudo apt-get install libstdc++6-4.5-doc   
