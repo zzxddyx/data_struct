42f7cbd415c30f89e4baac98e455b3583cd12380
zhaoxudong <zzxddyx@163.com>
2019-04-23 21:47:42 +0800

    新文件：   2/2.14.cpp

---
+++ CPP/prime/2/2.14.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: 2.14.cpp
+    > Author: BernieZhao
+    > Mail: berniezhao@ezfun.cn
+    > Created Time: 2019年04月23日 星期二 21时43分11秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+    int i = 100, sum = 0;
+    for (int i = 0; i != 10; ++i)
+        sum += i;
+
+    cout << i << sum << endl;
+
+    return 0;
+}
