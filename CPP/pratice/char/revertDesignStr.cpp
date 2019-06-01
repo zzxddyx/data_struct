023c726e5d418b3095320319a44ce8075a917845
nickzhao <nickzhao@ezfun.cn>
2019-06-01 15:05:43 +0800

    修改：      revertDesignStr.cpp

--- CPP/pratice/char/revertDesignStr.cpp
+++ CPP/pratice/char/revertDesignStr.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <stack>
 
 void swap(char* c1, char* c2)
 {
@@ -31,7 +32,7 @@ void revertDesignStr(char *str, char* token)
         if(*tmp == str[index])
         {
             end = str+index;
-            while(*tmp == str[index])
+            while(str[index] != '\0' && *tmp != '\0' && *tmp == str[index])
             {
                 --index;
                 tmp++;
@@ -58,13 +59,60 @@ void revertDesignStr(char *str, char* token)
     }
 }
 
+void revert(char* str, char* token)
+{
+    if(NULL == str || NULL == token)
+        return;
+
+    char* pstr = str;
+    char* ptmp = str;
+    char* ptok = token;
+    std::stack<char> cStack;
+
+    while(*pstr != '\0')
+    {
+        ptok = token;
+        ptmp = pstr;
+        while((*ptmp != '\0') && (*ptok != '\0') && (*ptmp == *ptok))
+        {
+            ptmp++;
+            ptok++;
+        }
+
+        if(*ptok == '\0')
+        {
+            for(char* p = ptmp-1; p >= pstr; --p)
+                cStack.push(*p);
+
+            pstr = ptmp;
+        }
+        else
+        {
+            cStack.push(*pstr);
+            pstr++;
+        }
+    }
+
+    int index = 0;
+    while(!cStack.empty())
+    {
+        char c = cStack.top();
+        cStack.pop();
+        *(str+index) = c;
+        index++;
+    }
+}
+
 int main(void)
 {
-    char str[] = "Welcome To Shanxi";
-    char token[] = "To Shan";
+    //char str[] = "Welcome To Shanxi";
+    //char token[] = "To Shan";
+    char str[] = "Welcome you, my friend";
+    char token[] = "you";
 
     std::cout << str << std::endl;
-    revertDesignStr(str, token);
+    //revertDesignStr(str, token);
+    revert(str, token);
     std::cout << str << std::endl;
 
     return 0;
