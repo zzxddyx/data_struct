129d80b3dd81460177c3845cbe2b853faaa4b647
zhaoxudong <zzxddyx@163.com>
2019-06-10 21:55:54 +0800

    新文件：   datastruct/doubleList.h 	新文件：   datastruct/list.h

---
+++ CPP/pratice/datastruct/doubleList.h
@@ -0,0 +1,22 @@
+#ifndef _DOUBLELIST_H_
+#define _DOUBLELIST_H_
+
+typedef struct LIST
+{
+    int data;
+    struct LIST *next;
+    struct LIST *prev;
+}NODE;
+
+NODE *create(int value);
+void connect(NODE *node1, NODE *node2);
+int length(NODE *phead);
+void print(NODE *phead);
+NODE *search(NODE *phead, int data);
+void insert(NODE *phead, int data);
+NODE *delets(NODE *phead, int data);
+// 有序双向循环链表的插入操作
+// 删除两个双向循环链表的相同结点
+
+
+#endif //_DOUBLELIST_H_
---
+++ CPP/pratice/datastruct/list.h
@@ -0,0 +1,23 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+typedef struct LIST
+{
+    int data;
+    struct LIST *next;
+}NODE;
+
+NODE *create(int n); // 创建
+void connect(NODE *pcur, NODE *pnext); // 结点链接
+int length(NODE *phead); // 求长度
+void print(NODE *phead); // 打印
+NODE *search(NODE *phead, int pos); // 但链表结点的查找
+NODE *insert(NODE *phead, int data); // 结点的插入
+NODE *delets(NODE *phead, int pos); // 结点删除
+NODE *reverse(NODE *phead); // 翻转
+NODE *search_mid(NODE *phead); // 寻找中间元素
+NODE *sort(NODE *phead); // 排序
+bool isLoop(NODE *phead, NODE **start); // 判断是否有环
+NODE *merge(NODE *phead1, NODE *phead2); // 两个有序链表合并
+
+#endif //_LIST_H_
